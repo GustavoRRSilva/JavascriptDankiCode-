4b9c5135c836a0b841a876d1a1505b985ca6ea42
Gustavo <gstvrrds@gmail.com>
2023-02-22 09:43:25 -0300

    fim do modulo orientação a objetos com js

--- data.js
+++ data.js
@@ -7,6 +7,11 @@ alert(data.getHours());//retorna que horas são(somente as horas, sem os minutos
 alert(data.getFullYear())//Retorna o ano atual
 */
 
-var dataNasc = new Date("09/08/2023")
-var dataAtual = new Date("09/10/2023");
-console.log((dataAtual.getTime() - dataNasc.getTime()) / (1000 * 3600 * 24));//Diz o tempo em dias
+//mm-dd-yyyy
+
+var date = new Date('02/22/2023')
+var dateNasc = new Date('09/13/2004')
+
+var diferenca = (date.getTime() - dateNasc.getTime()) /(1000 * 60 * 60 * 24)
+
+alert(diferenca)
